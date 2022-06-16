09e0ec88d0261e6595eae626f1b0613bab213d7f
Jotrol <ilichevdaniil@gmail.com>
2022-06-16 13:45:55 +0300

    Определена структура MRZ кода Созданы структуры, по которым можно производить разбор MRZ зоны

--- BAC_reader/main.cpp
+++ BAC_reader/main.cpp
@@ -2,16 +2,31 @@
 #include "CardReader.hpp"
 using namespace std;
 
+struct UpperMRZ {
+	char docType[2];
+	char issuingState[3];
+	char name[31];
+};
+
+struct DownerMRZ {
+	char passNum[9];
+	char controlZifr1[1];
+	char nationality[3];
+	char birthdate[6];
+	char controlZifr2[1];
+	char sex[1];
+	char expires[6];
+	char controlZifr3[1];
+	char facultative[8];
+};
 
 
 //INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow) {
 int main() {
 	setlocale(LC_ALL, "rus");
 
-	string mrz_line1 = "";
-	string mrz_line2 = "";
-	cout << "Введите строку MRZ1: "; cin >> mrz_line1;
-	cout << "Введите строку MRZ2: "; cin >> mrz_line2;
+	UpperMRZ* upper = (UpperMRZ*)"P<RUSSMIRNOVA<<VALENTINA<<<<<<<<<<<<<<<<<<<<";
+	DownerMRZ* downer = (DownerMRZ*)"5200000001RUS8008046F0902274<<<<<<<<<<<06";
 	
 	/* А дальше строки распознаются и отдают валидный словарь */
 
