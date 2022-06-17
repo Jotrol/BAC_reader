e05d314b3f289c04d5fa9eae379ddac8bfef1846
Jotrol <ilichevdaniil@gmail.com>
2022-06-17 12:38:11 +0300

    Написан тестовый код для работы с картой Устанавливается защищённое соединение, пока без передачи защищённых сообщений

---
+++ BAC_reader/MRZDecoder.hpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+/* Простенький класс для выделения нужных полей из второй строки MRZ кода */
+class DecoderMRZLine2 {
+private:
+	/* Везде нужна контрольная цифра */
+	char passNumRaw[10];
+	char birthDate[7];
+	char expireDate[7];
+public:
+	DecoderMRZLine2(string mrzLine) {
+		char buffer[40] = { 0 };
+
+		/* Выполняем процесс считывания */
+		istringstream ss(mrzLine);
+		ss.read(passNumRaw, sizeof(passNumRaw));
+		ss.read(buffer, 3); /* Пропуск гражданства */
+		ss.read(birthDate, sizeof(birthDate));
+		ss.read(buffer, 1); /* Пропуск пола */
+		ss.read(expireDate, sizeof(expireDate));
+	}
+
+	vector<unsigned char> ComposeData() {
+		char dataStr[24] = { 0 };
+		memcpy(dataStr, passNumRaw, 10);
+		memcpy(dataStr + 10, birthDate, 7);
+		memcpy(dataStr + 17, expireDate, 7);
+
+		vector<unsigned char> data(24, 0);
+		memcpy(data.data(), dataStr, 24);
+		return data;
+	}
+};
