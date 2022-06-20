1eb9b2638b57210c31752363d2ea483909f799f1
Jotrol <ilichevdaniil@gmail.com>
2022-06-20 17:02:11 +0300

    Налажено взаимодействие между Windows GDI+ и библиотекой для работы с изображениями FreeImage

--- BAC_reader/main.cpp
+++ BAC_reader/main.cpp
@@ -3,30 +3,81 @@
 #include "Crypto.hpp"
 #include "CardReader.hpp"
 #include "MRZDecoder.hpp"
-#include "FreeImage.h"
-#pragma comment(lib, "FreeImage.lib")
+
+#include "FreeImagePlus.h"
+#pragma comment(lib, "FreeImagePlus.lib")
 using namespace std;
 
+#include <objidl.h>
+#include <gdiplus.h>
+#include <Shlwapi.h>
 
-//INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow) {
-int main() {
-	srand(time(NULL));
-	setlocale(LC_ALL, "rus");
+using namespace Gdiplus;
+#pragma comment (lib, "Gdiplus.lib")
+#pragma comment (lib, "Shlwapi.lib")
 
-	FreeImage_Initialise();
-	FIBITMAP* bitmap = FreeImage_Allocate(640, 480, 24);
-	RGBQUAD color = { 0 };
-	for (int i = 0; i < 640; i += 1) {
-		for (int j = 0; j < 480; j += 1) {
-			color.rgbRed = 0;
-			color.rgbGreen = (double)i / 640 * 255.0;
-			color.rgbBlue = (double)j / 480 * 255.0;
-			FreeImage_SetPixelColor(bitmap, i, j, &color);
-		}
-	}
+Gdiplus::Bitmap* image = nullptr;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_PAINT:
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        
+        Graphics graphics(hdc);
+        graphics.DrawImage(image, 0, 0);
 
-	FreeImage_Save(FIF_PNG, bitmap, "filename.png", 0);
-	FreeImage_DeInitialise();
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
 
-	return 0;
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR pCmdLine, int nCmdShow) {
+    ULONG_PTR token;
+    GdiplusStartupInput input = { 0 };
+    input.GdiplusVersion = 1;
+    GdiplusStartup(&token, &input, NULL);
+
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    RegisterClass(&wc);
+
+    fipImage fipimage;
+    if (!fipimage.load("FaceImage.jp2")) {
+        MessageBox(nullptr, L"Не загрузилось изображение", L"Ошибка", MB_OK | MB_ICONERROR);
+    }
+
+    HWND hwnd = CreateWindow(CLASS_NAME, L"Learn to Program Windows", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, fipimage.getWidth(), fipimage.getHeight() + 38,NULL,NULL,hInstance,NULL);
+    if (!hwnd) {
+        cerr << "Can't create window!" << endl;
+        GdiplusShutdown(token);
+        return 1;
+    }
+
+    BITMAPINFO bmi = { 0 };
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = fipimage.getWidth();
+    bmi.bmiHeader.biHeight = fipimage.getHeight();
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biCompression = BI_RGB;
+    bmi.bmiHeader.biBitCount = fipimage.getBitsPerPixel();
+    image = new Gdiplus::Bitmap(&bmi, fipimage.accessPixels());
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    
+    return 0;
 }
