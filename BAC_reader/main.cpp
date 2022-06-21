1f89d3a3d35d3239c745f02fe44a076726d7f6c8
Jotrol <ilichevdaniil@gmail.com>
2022-06-21 13:18:16 +0300

    Окно отображения фотографии было инкапсулировано в класс Был создан класс, реализующий возможность отображения фотографии внутри окна. Была добавлена возможность открыть другое изображение в этом окне

--- BAC_reader/main.cpp
+++ BAC_reader/main.cpp
@@ -8,36 +8,111 @@
 #pragma comment(lib, "FreeImagePlus.lib")
 using namespace std;
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	static fipImage* fipimage = nullptr;
-	static BITMAPINFO bmi = { 0 };
+LRESULT CALLBACK ImageWindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+class ImageWindow {
+private:
+	fipImage image;
+	BITMAPINFO bmi;
+	HWND hWnd;
+	
+	const wchar_t* ImageClassName = L"ImageWindow";
+	const DWORD ImageWindowStyle = WS_CHILD | WS_OVERLAPPED;
 
-	switch (uMsg) {
-	case WM_SHOWWINDOW:
-		fipimage = (fipImage*)GetWindowLongPtrW(hwnd, GWLP_USERDATA);
+	bool RegisterImageWindowClass(HINSTANCE hInst) {
+		WNDCLASS wc = { 0 };
+		wc.hInstance = hInst;
+		wc.lpfnWndProc = ImageWindowProc;
+		wc.lpszClassName = ImageClassName;
 
+		return RegisterClass(&wc);
+	}
+public:
+	ImageWindow(HINSTANCE hInst, HWND hParentWnd, INT x, INT y, const char* filename) {
+		if (!image.load(filename)) {
+			throw std::exception("Ошибка: не удалось загрузить фотографию");
+		}
+		if (!image.convertTo24Bits()) {
+			throw std::exception("Ошибка: не удалось конвертировать фотографию");
+		}
+
+		DWORD w = image.getWidth();
+		DWORD h = image.getHeight();
 		bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-		bmi.bmiHeader.biWidth = fipimage->getWidth();
-		bmi.bmiHeader.biHeight = fipimage->getHeight();
+		bmi.bmiHeader.biWidth = w;
+		bmi.bmiHeader.biHeight = h;
 		bmi.bmiHeader.biPlanes = 1;
 		bmi.bmiHeader.biCompression = BI_RGB;
-		bmi.bmiHeader.biBitCount = fipimage->getBitsPerPixel();
-		return 0;
+		bmi.bmiHeader.biBitCount = image.getBitsPerPixel();
+
+		hWnd = CreateWindow(ImageClassName, L"", ImageWindowStyle, x, y, w, h, hParentWnd, nullptr, hInst, nullptr);
+		if (!hWnd) {
+			if (!RegisterImageWindowClass(hInst)) {
+				throw std::exception("Ошибка: не удалось зарегистрировать класс окна для изображений");
+			}
+			hWnd = CreateWindow(ImageClassName, L"", ImageWindowStyle, x, y, w, h, hParentWnd, nullptr, hInst, nullptr);
+			if (!hWnd) {
+				throw std::exception("Ошибка: не удалось создать окно");
+			}
+		}
+
+		SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)this);
+		UpdateWindow(hWnd);
+		ShowWindow(hWnd, 1);
+	}
+
+	bool reloadImage(const char* filename) {
+		image.clear();
+
+		if (!image.load(filename)) {
+			return false;
+		}
+		if (!image.convertTo24Bits()) {
+			image.clear();
+			return false;
+		}
+
+		DWORD w = image.getWidth();
+		DWORD h = image.getHeight();
+		bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+		bmi.bmiHeader.biWidth = w;
+		bmi.bmiHeader.biHeight = h;
+		bmi.bmiHeader.biPlanes = 1;
+		bmi.bmiHeader.biCompression = BI_RGB;
+		bmi.bmiHeader.biBitCount = image.getBitsPerPixel();
+
+		MoveWindow(hWnd, 0, 0, w, h, false);
+		InvalidateRect(hWnd, nullptr, true);
+		return true;
+	}
+	
+	LRESULT CALLBACK handleMessage(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+		switch (message) {
+		case WM_PAINT:
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hWnd, &ps);
+			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+			SetDIBitsToDevice(hdc, 0, 0, image.getWidth(), image.getHeight(), 0, 0, 0, image.getHeight(), image.accessPixels(), &bmi, DIB_RGB_COLORS);
+			EndPaint(hWnd, &ps);
+			return 0;
+		}
+
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+};
+LRESULT CALLBACK ImageWindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	ImageWindow* imageWindow = (ImageWindow*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+
+	switch (message) {
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
-	case WM_PAINT:
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-		SetDIBitsToDevice(hdc, 0, 0, fipimage->getWidth(), fipimage->getHeight(), 0, 0, 0, fipimage->getHeight(), fipimage->accessPixels(), &bmi, DIB_RGB_COLORS);
-		EndPaint(hwnd, &ps);
-		return 0;
 	}
 
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	return (imageWindow ? imageWindow->handleMessage(hWnd, message, wParam, lParam)
+		: DefWindowProc(hWnd, message, wParam, lParam));
 }
 
+ImageWindow* image = nullptr;
 LRESULT CALLBACK ParentProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
 	case WM_DESTROY:
@@ -48,39 +123,11 @@ LRESULT CALLBACK ParentProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 		HDC hdc = BeginPaint(hWnd, &ps);
 		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 		EndPaint(hWnd, &ps);
+		return 0;
 	}
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
-#define CLASS_NAME L"Sample Window Class"
-
-HWND createImageWindow(HINSTANCE hInst, const char* filename, HWND parentWnd, fipImage& image) {
-	WNDCLASS wc = {};
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInst;
-	wc.lpszClassName = CLASS_NAME;
-	if (!RegisterClass(&wc)) {
-		MessageBox(nullptr, L"Не удалось зарегистрировать класс окна", L"Ошибка", MB_OK | MB_ICONERROR);
-		return nullptr;
-	}
-
-	if (!image.load(filename)) {
-		MessageBox(nullptr, L"Не загрузилось изображение", L"Ошибка", MB_OK | MB_ICONERROR);
-		return nullptr;
-	}
-	image.convertTo24Bits();
-
-	HWND hWnd = CreateWindow(CLASS_NAME, L"Learn to Program Windows", ~WS_BORDER & ~WS_THICKFRAME & (WS_CHILD | WS_OVERLAPPEDWINDOW), CW_USEDEFAULT, CW_USEDEFAULT, image.getWidth(), image.getHeight(), parentWnd, NULL, hInst, NULL);
-	if (!hWnd) {
-		MessageBox(nullptr, L"Не удалось создать окно", L"Ошибка", MB_OK | MB_ICONERROR);
-		return nullptr;
-	}
-
-	SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)&image);
-	UpdateWindow(hWnd);
-	ShowWindow(hWnd, 1);
-	return hWnd;
-}
 HWND createParentWindow(HINSTANCE hInst) {
 	WNDCLASS wc = {};
 	wc.lpfnWndProc = ParentProc;
@@ -103,10 +150,8 @@ HWND createParentWindow(HINSTANCE hInst) {
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR pCmdLine, int nCmdShow) {
-	fipImage image;
-
 	HWND parentWindow = createParentWindow(hInstance);
-	HWND imageWindow = createImageWindow(hInstance, "Faceimage.jp2", parentWindow, image);
+	image = new ImageWindow(hInstance, parentWindow, 0, 0, "Faceimage.jp2");
 
 	MSG msg = { 0 };
 	while (GetMessage(&msg, NULL, 0, 0)) {
